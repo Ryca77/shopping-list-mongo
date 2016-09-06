f5cc4aaf2d8d9974c5d5883af9fbc1ac3315cb7b
Ryan Carter <ryan.carter77@gmail.com>
2016-09-06 11:03:48 +0000

    added mlab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
                         (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://<dbuser>:<dbpassword>@ds019996.mlab.com:19996/mongo-shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
